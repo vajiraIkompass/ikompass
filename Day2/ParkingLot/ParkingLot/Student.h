d8395705e61a90bd7ca772c03e0ca1499db20837
Vajira <COSMIC_Vajira@CosmicMacBook1.local>
2015-07-05 13:55:18 +0800

    Committing updates from local to remote repository

---
+++ Day2/ParkingLot/ParkingLot/Student.h
@@ -0,0 +1,13 @@
+//
+//  Student.h
+//  ParkingLot
+//
+//  Created by Vajira on 5/7/15.
+//  Copyright (c) 2015 cosmic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Student : NSObject
+
+@end
---
+++ Day2/ParkingLot/ParkingLot/Student.m
@@ -0,0 +1,13 @@
+//
+//  Student.m
+//  ParkingLot
+//
+//  Created by Vajira on 5/7/15.
+//  Copyright (c) 2015 cosmic. All rights reserved.
+//
+
+#import "Student.h"
+
+@implementation Student
+
+@end
